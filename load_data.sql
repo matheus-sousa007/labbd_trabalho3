cbf277ee7a8d5c43a09c158b2f51f7b0c1e2855c
matheus-sousa007 <mvsousa5@gmail.com>
2025-04-06 21:52:38 -0300

    Fixing sql commands to insert data into database.

--- load_data.sql
+++ load_data.sql
@@ -1,30 +1,14 @@
-DO $$
-DECLARE psql_mode TEXT;
-BEGIN
-    -- Obtendo a variável do PSQL (psql_mode) pra determinar que o script está sendo rodado no psql. Se estiver, ele usa o comando \COPY em vez do COPY
-    BEGIN
-        SELECT :'PSQL_MODE' INTO psql_mode;
-    EXCEPTION WHEN OTHERS THEN
-        psql_mode := NULL;
-    END;
-    IF psql_mode IS NOT NULL THEN
-        -- Comandos a serem executados dentro do psql 
-        RAISE NOTICE '';
-    ELSE
-        -- Comandos SQL Normais
-        EXECUTE 'COPY STATUS FROM ’./LabBD/status.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY SEASONS FROM ’./LabBD/seasons.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY COUNTRIES FROM ’./LabBD/countries.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY AIRPORTS FROM ’./LabBD/airports.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY GEOCITIES15K FROM ’./LabBD/Cities15000.tsv’ WITH (FORMAT CSV, DELIMITER ’\t’, HEADER true)';
-        EXECUTE 'COPY CIRCUITS FROM ’./LabBD/circuits.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY CONSTRUCTORS FROM ’./LabBD/constructors.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY DRIVER FROM ’./LabBD/drivers.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY RACES FROM ’./LabBD/races.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY DRIVERSTANDINGS FROM ’./LabBD/driver_Standings.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY LAPTIMES FROM ’./LabBD/lap_Times.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY PITSTOPS FROM ’./LabBD/pit_Stops.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY QUALIFYING FROM ’./LabBD/qualifying.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-        EXECUTE 'COPY RESULTS FROM ’./LabBD/results.csv’ WITH (FORMAT CSV, DELIMITER ’,’, HEADER true)';
-    END IF;
-END $$
+\COPY status FROM '/app/LabBDData/status.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY seasons FROM '/app/LabBDData/seasons.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY countries FROM '/app/LabBDData/countries.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY airports FROM '/app/LabBDData/airports.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY geocities15k FROM '/app/LabBDData/Cities15000.tsv' WITH (FORMAT CSV, DELIMITER '\t', HEADER true);
+\COPY circuits FROM '/app/LabBDData/circuits.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY constructors FROM '/app/LabBDData/constructors.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY driver FROM '/app/LabBDData/drivers.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY races FROM '/app/LabBDData/races.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY driverstandings FROM '/app/LabBDData/driver_Standings.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY laptimes FROM '/app/LabBDData/lap_Times.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY pitstops FROM '/app/LabBDData/pit_Stops.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY qualifying FROM '/app/LabBDData/qualifying.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\COPY results FROM '/app/LabBDData/results.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER true);
